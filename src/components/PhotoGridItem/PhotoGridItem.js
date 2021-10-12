92b3fe99b624129b65c1a45766a6d6f72e243d76
shduff <shaunalynnduffy@gmail.com>
2021-10-12 16:51:25 -0400

    Module 6 work

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -5,7 +5,25 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source 
+            type="image/avif" 
+            srcSet={`
+              ${src} 1x,
+              ${src.replace('.jpg','@2x.avif')} 2x,
+              ${src.replace('.jpg','@3x.avif')} 3x
+            `}
+          />
+          <source 
+            type="image/jpg" 
+            srcSet={`
+              ${src} 1x,
+              ${src.replace('.jpg','@2x.jpg')} 2x,
+              ${src.replace('.jpg','@3x.jpg')} 3x              
+            `} 
+          />
+          <Image alt={alt} src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,12 +46,17 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit:cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
+  /*display: flex;
   flex-wrap: wrap;
-  gap: 8px;
+  gap: 8px;*/
+  overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap;
+  padding:4px 0;
 `;
 
 const Tag = styled.li`
@@ -42,6 +65,11 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  display:inline;
+
+  &:not(:last-of-type) {
+    margin-right:8px;
+  }
 `;
 
 export default PhotoGridItem;
